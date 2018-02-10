3b0e7411e93519feb7ecc4e13446da399a656708
Phillip <phillipables1@gmail.com>
2018-02-10 18:16:20 -0500

    main class extends list Activity with listview id android:id/list in out main activity layout:

--- ListActivity/app/src/main/java/com/example/techtron/listactivity/MainActivity.java
+++ ListActivity/app/src/main/java/com/example/techtron/listactivity/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.techtron.listactivity;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
     //define a data source
     String[] days = {"Sunday", "Monday", "Tuesday", "Wenseday", "Thurseday", "Friday", "Saturday"};
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
