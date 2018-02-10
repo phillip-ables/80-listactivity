e30dc170416a6e636f012a33af34ef0bb8b99253
Phillip <phillipables1@gmail.com>
2018-02-10 18:19:07 -0500

    reference listView var equal to getListView which is out android:id/list ListView

--- ListActivity/app/src/main/java/com/example/techtron/listactivity/MainActivity.java
+++ ListActivity/app/src/main/java/com/example/techtron/listactivity/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.techtron.listactivity;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
     //define a data source
     String[] days = {"Sunday", "Monday", "Tuesday", "Wenseday", "Thurseday", "Friday", "Saturday"};
+    //we need a reference to the list view
+    ListView l;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //now we dont have to say find id by blah blah blah
+        l = getListView();
+        //adapter data source
+        
     }
 
 
